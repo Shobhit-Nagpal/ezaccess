ec761106a133eec27e357757e6aae924fb5d80bd
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-26 16:18:51 +0530

    Added custom font

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
   const { data } = useData();
 
   const { showForm, setShowForm } = useForm();
